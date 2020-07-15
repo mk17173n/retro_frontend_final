b55e20b13662fa2500ddc4990e3aa8dff0e0ada7
mk17173n <Monsur.Khan@gmail.com>
2020-07-15 13:57:49 -0400

    Reset CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import './App.css';
 import Form from './components/Form'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
